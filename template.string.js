e350284b56729052061c9b8e5e9c170a56882c34
CoderMahabub <codermahabub@gmail.com>
2021-08-23 01:31:55 +0600

    30-5 Template string, multiple line string, dynamic string

---
+++ template.string.js
@@ -0,0 +1,23 @@
+const priya = 'Asif Akbar';
+const meye = "Meye tumi ki dukkho chino";
+const kobita = `kobita tumi sopno charini hoye`;
+//First Shubidha
+const multilineNew = `This is my first line
+second line
+third line
+multiline`;
+
+// console.log(multilineNew);
+const friends = ['abu', 'babul', 'kabul'];
+const count = 5;
+const old = '<h3 class="friend-name">Friend 5</h3>'
+const old2 = '<h3 class="friend-name">Friend' + count + '</h3> ';
+const new1 = `<h3 class="friend-name">Friend ${friends.length * 500}. He lives in Dhaka</h3>`;
+console.log(new1);
+
+//Dynamically Use
+const first = "Umme";
+const last = "Habiba";
+// const PuraNam = first + ' ' + last;
+const puranam = `${first} ${last}`;
+console.log(puranam);
